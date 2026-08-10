e6add53826389dde15f07ab148786749e517e074
JackOliver08 <olive200@purdue.edu>
2026-08-09 20:21:13 -0400

    tceti characteristic values

--- sophieho/load_characteristic_values_tceti_star.m
+++ sophieho/load_characteristic_values_tceti_star.m
@@ -4,25 +4,22 @@ function [tceti_star] = load_characteristic_values_tceti_star()
     tceti_star.t = sqrt(tceti_star.mu^-1 * tceti_star.l^3); %[s]
     tceti_star.v = tceti_star.l/tceti_star.t; %[km/s]
     tceti_star.a = tceti_star.v/tceti_star.t;%[km/s^2]
-end
-
-function [tceti_f] = load_characteristic_values_tceti_e()
     tceti_f.mu = 1566504.9301; %[km3 / s2] NASA data
     tceti_f.a = 199564000; %[km] NASA data
     tceti_f.e = 0.16; % NASA data
     tceti_f.theta_star = 65; %[km/s] 
-end
-
-function [tceti_g] = load_characteristic_values_tceti_g()
-    tceti_g.mu = 470350.215061; %[ km3 / s2]
+    tceti_g.mu = 470350.215061; %[f km3 / s2]
     tceti_g.a = 19896517; %[km]
     tceti_g.e = 0.06;
     tceti_g.theta_star = 0; %[km/s]
-end
-
-function [tceti_h] = load_characteristic_values_tceti_h()
-    tceti_h.mu = 474336.233832; %[ km3 / s2]
+    tceti_h.mu = 474336.233832; %[km3 / s2]
     tceti_h.a = 36352283; %[km]
     tceti_h.e = 0.23;
-    tceti_t.theta_star = 143; %[km/s]
+    tceti_h.theta_star = 143; %[km/s]
 end
+
+planet_h = [tceti_h.mu, tceti_h.a, tceti_h.e, tceti_h.theta_star]
+planet_g = [tceti_g.mu, tceti_g.a, tceti_g.e, tceti_g.theta_star]
+planet_f = [tceti_f.mu, tceti_g.a, tceti_g.e, tceti_g.theta_star]
+
+
